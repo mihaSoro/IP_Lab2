9f52984e50433d381cc6343b5ee53ce283a6d1b9
Virgil <virgil.cervicescu@gmail.com>
2014-10-26 23:31:59 +0200

    Initial commit

---
+++ src/mta/se/lab/basic/Main.java
@@ -0,0 +1,18 @@
+package mta.se.lab.basic;
+
+/**
+ * Created by John on 26/10/2014.
+ *
+ * This the main class of the project. It is also the entry point of the application.
+ */
+public class Main {
+
+    /**
+     * The entry point to the project
+     *
+     * @param args  The arguments to the executable
+     */
+    public static void main(String[] args) {
+        System.out.println("This is easy");
+    }
+}
